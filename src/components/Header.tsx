74596c8ffbad4d0470b8bac0a6992e94c4828cd2
HEADCUBE <omarkhalfi2010@gmail.com>
2024-02-17 19:14:28 +0100

    hahah

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,28 +3,30 @@ import LogoutButton from "@/components/LogoutButton";
 import { authOptions } from "@/lib/authOptions";
 import { getServerSession } from "next-auth";
 import Link from "next/link";
+import Logo from "/public/logo.jpg";
 
 export default async function Header() {
   const session = await getServerSession(authOptions);
   return (
     <header className="bg-gray-200 p-4 px-8">
-      <div className="flex justify-between items-center">
-        <Link href="/" className="logo">Task-UIC</Link>
-        <div>
-          {session && (
-            <>
-              Hello, {session?.user?.name}
-              <LogoutButton />
-            </>
-          )}
-          {!session && (
-            <>
-              Not logged in
-              <LoginButton />
-            </>
-          )}
+        <div className="flex justify-between items-center">
+            <img src={Logo.src} alt="Logo" className="h-8 w-auto"/>
+            <Link href="/" className="logo">Task-UIC</Link>
+            <div>
+                {session && (
+                    <>
+                        Hello, {session?.user?.name}
+                        <LogoutButton/>
+                    </>
+                )}
+                {!session && (
+                    <>
+                        Not logged in
+                        <LoginButton/>
+                    </>
+                )}
+            </div>
         </div>
-      </div>
     </header>
-  );
+  );
 }
